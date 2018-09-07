6c1736de62ddda4bb68212df433d1968784edb76
jacquetwong <jacquet@oneskyapp.com>
2018-09-07 09:44:27 +0800

    Update header

--- onesky-conductor/localization-script-in-build-phases.sh
+++ onesky-conductor/localization-script-in-build-phases.sh
@@ -1,2 +1,2 @@
 # Run script in Build Phases
-curl -H "Authorization: Bearer ${ONESKY_API_KEY}" -s https://app-api.onesky.app/v1/apps/${ONESKY_APP_ID}/build-scripts?platformId=ios | sh
+curl -H "Authorization: Bearer ${ONESKY_API_KEY}" -H "Platform: ios" -H "SDK-Version: 0.0.1" -s https://app-api.onesky.app/v1/apps/${ONESKY_APP_ID}/build-scripts | sh
